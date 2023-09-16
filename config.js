0bbcefb038e35aa53a42d2e788e12c2b59d92727
levislai <143705527+levislai@users.noreply.github.com>
2023-09-16 14:39:35 +0000

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
+global.Owner = ["22545752448","22545752448"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["22545752448","22545752448"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["22545752448"];
+global.OwnerName = "✯᚛ƐRRφR᚜⁴0⁴乄᚛・";
 global.BotName = "XLICON-MD";
 global.packname = "X-2.0 Bot";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.author = "By: ✯᚛ƐRRφR᚜⁴0⁴乄᚛・";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/EjsQvJNcFGVCSfaBEIxZm2"; 
 
@@ -33,13 +33,13 @@ global.sessionName = "session";                          //Do not change.
 
 
 //
-global.prefa = ["."];                                    //Default prefix here.
+global.prefa = ["#"];                                    //Default prefix here.
 global.openAiAPI = "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";
 
 
 //
-global.location = "Dhaka, Bangladesh";                   
-global.reactmoji = "✅";
+global.location = "côte d'ivoire, Riveria golf";                   
+global.reactmoji = "🦦";
 global.themeemoji = "💖";
 global.vidmenu = { url: 'https://tenor.com/view/jujutsu-kaisen0-yuta-okkotsu-gif-26767662' };
 global.websitex = "https://github.com/ahil15";
